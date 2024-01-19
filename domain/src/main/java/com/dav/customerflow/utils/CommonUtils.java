2ca7e033154d7b901becc52490cd88f3b6317eaf
dnguyen292 <ducminh.nguyen@dxc.com>
2024-01-20 00:48:30 +0700

    Update Java time module

--- domain/src/main/java/com/dav/customerflow/utils/CommonUtils.java
+++ domain/src/main/java/com/dav/customerflow/utils/CommonUtils.java
@@ -2,6 +2,7 @@ package com.dav.customerflow.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -12,7 +13,9 @@ public class CommonUtils {
 
     public static String toJsonString(Object obj) {
         try {
-            return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
         } catch (JsonProcessingException e) {
             log.error("Failed to extract {}", e.getMessage());
             return null;
